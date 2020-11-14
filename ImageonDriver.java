83e5e17109d29764ec7733d7736b5c088d8a37c8
LeviStone22 <walkerls@etsu.edu>
2020-11-13 21:23:39 -0500

    edited playerUI and added driver to program
    octopus

---
+++ ImageonDriver.java
@@ -0,0 +1,25 @@
+import javax.swing.JOptionPane;
+
+
+
+public class ImageonDriver 
+{
+
+
+    public static void main(String[] args) 
+    {
+        JOptionPane.showMessageDialog(null, "Welcome to Imageon. \nA text based adventure game by Levi Walker and Samuel Pinnex.");
+        
+        JOptionPane.showMessageDialog(null, "As a child, Thomas Stone was a very creative boy. \nHis imagination took him to rocky cliffs, undersea coral reefs, and sprawling dungeons with luxuries to discover. \nOur adventure today, takes us to the latter.");
+    
+    
+
+
+
+        
+        System.exit(0);
+    }
+
+
+
+}
--- PlayerUI.java
+++ PlayerUI.java
@@ -1,6 +1,34 @@
-
+import javax.swing.JOptionPane;
 
 public class PlayerUI 
 {
-    
+    int playerHealth = 100;
+    int playerDMG = 0;
+
+
+    public PlayerUI()
+    {
+        JOptionPane.showMessageDialog(null, "HEALTH: " + playerHealth);
+        JOptionPane.showMessageDialog(null, "\nCURRENT DMG: " + playerDMG);
+    }
+
+
+    public int getHealth()
+    {
+        return playerHealth;
+    }
+
+    public int getDMG()
+    {
+        return playerDMG;
+    }
+
+    public getInventory() // question for erin, should we create the array list in here or in items and refer to it here (for the player's inventory)
+    {
+        
+    }
+
+
 }
+
+
