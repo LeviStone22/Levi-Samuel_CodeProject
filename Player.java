b23751b2823470fcfaad73f2e4ffa293d1586e79
LeviStone22 <walkerls@etsu.edu>
2020-11-21 21:38:28 -0500

    commented playerUI and fleshed out some player commenting
    Sending off to sam

--- Player.java
+++ Player.java
@@ -22,28 +22,28 @@ public class Player
 
     public void addWeapon()
     {
-        weapon = new Item("Thomas' Blade", 16, 0);
-        inventory.add(weapon);
+        weapon = new Item("Thomas' Blade", 16, 0); // creates a new item that acts as a weapon for the user
+        inventory.add(weapon); // adds the item to the inventory (array list)
     }
 
     public void addPotion()
     {
-        potion = new Item("Health Potion", 0, 40);
-        inventory.add(potion);
+        potion = new Item("Health Potion", 0, 40); // creates a new item that acts as a health potion
+        inventory.add(potion); // adds the item to the inventory (array list)
     }
 
     public String showInventory()
     {
-        String inventoryStock = "";
-        inventoryStock += "~-~-~-~-~ Thomas' Inventory ~-~-~-~-~\n";
-        inventoryStock += "There are " + inventory.size() + " items in your inventory.\n\n";
+        String inventoryStock = ""; // declares a blank String message named inventoryStock
+        inventoryStock += "~-~-~-~-~ Thomas' Inventory ~-~-~-~-~\n"; // inventory header message that displays to the player
+        inventoryStock += "There are " + inventory.size() + " items in your inventory.\n\n"; // displays the array list's (inventory) size within a message for the player
 
-        for(Item item : inventory)
+        for(Item item : inventory) // for each loop that, for each item in the inventory array list, prints out the toString for those items in the inventory display
         {
-            inventoryStock += item.toString() + "\n\n";
+            inventoryStock += item.toString() + "\n\n"; // this is triggered by pressing "2" on the driver menu
         }
 
-        return inventoryStock;
+        return inventoryStock; // returns inventoryStock
 
     }
 
--- PlayerUI.java
+++ PlayerUI.java
@@ -1,11 +1,38 @@
 import javax.swing.JOptionPane;
 
+/**
+* ---------------------------------------------------------------------------
+* File name: PlayerUI.java
+* Project name: LS_Code
+* ---------------------------------------------------------------------------
+* Creator's name and email: Levi Walker, walkerls@etsu.edu
+* Course: CSCI 1250-900
+* Creation Date: November 9, 2020
+* ---------------------------------------------------------------------------
+**/
+
+/**
+ * This is the class that holds the meat of PlayerUI. This isn't a totally utilized class in our program, but we
+ * consider it a "beta-tested" class in our program, that just didn't make it to be implemented in our final program.
+ * 
+ * Created by Levi Walker
+ * Created on 11/9/2020
+ */
 public class PlayerUI 
 {
-    int playerHealth;
-    int playerDMG;
+    int playerHealth; // declares playerHealth as an int variable
+    int playerDMG; // declares playerDMG as an int variable
 
 
+    /**
+     * This is the constructor of the PlayerUI class, which constructs (or was intended to construct)
+     * a PlayerUI instance for the player or user of this program. It would have included a toString method
+     * that would have shown all of the data of the game (the encounter data, player stats data, etc.) to the user
+     * while they were interacting with the program's systems.
+     * 
+     * Created Levi Walker
+     * Created on 11/9/2020
+     */
     public PlayerUI()
     {
         JOptionPane.showMessageDialog(null, "HEALTH: " + playerHealth);
@@ -13,14 +40,30 @@ public class PlayerUI
     }
 
 
+    /**
+     * This is a getter method that retrives and returns the health variable associated with the player. 
+     * This can be used wherever the program needs it to be used.
+     * 
+     * Created by Levi Walker
+     * Created on 11/9/2020
+     * @return
+     */
     public int getHealth()
     {
-        return playerHealth;
+        return playerHealth; // returns playerHealth
     }
 
+    /**
+     * This is a getter method that retrives and returns the damage variable associated with the player. 
+     * This can be used wherever the program needs it to be used.
+     * 
+     * Created by Levi Walker
+     * Created on 11/9/2020
+     * @return
+     */
     public int getDMG()
     {
-        return playerDMG;
+        return playerDMG; // returns playerDMG
     }
 
 
